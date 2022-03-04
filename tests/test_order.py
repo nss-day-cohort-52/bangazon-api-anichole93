5510922c837ed8e5f5b510b57f195530f9a005b9
Ashley Dickey <adickey93@gmail.com>
2022-03-04 13:56:14 -0600

    tests

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,11 +1,16 @@
+from datetime import date, datetime
 from rest_framework import status
 from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from django.core.management import call_command
 from django.contrib.auth.models import User
-
-from bangazon_api.models import Order, Product
+from bangazon_api.management.commands.seed_db import Command
+import random
+from bangazon_api.models import Order, Product, OrderProduct
+from bangazon_api.models import payment_type
+from bangazon_api.models.category import Category
 from bangazon_api.models.payment_type import PaymentType
+from bangazon_api.models.store import Store
 
 
 class OrderTests(APITestCase):
@@ -16,9 +21,11 @@ class OrderTests(APITestCase):
         call_command('seed_db', user_count=1)
         self.user = User.objects.filter(store=None).first()
         self.token = Token.objects.get(user=self.user)
-
+        self.payment_type = PaymentType.objects.get(customer=self.user)
+        
         self.order1 = Order.objects.create(
-            user=self.user
+            user=self.user,
+            payment_type=self.payment_type
         )
 
         self.client.credentials(
@@ -34,4 +41,32 @@ class OrderTests(APITestCase):
     def test_delete_order(self):
         response = self.client.delete(f'/api/orders/{self.order1.id}')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_complete_order(self):
+        order = Order.objects.first()
+        payment_type = PaymentType.objects.get(customer=order.user)
+        data = {
+            "created_on": order.created_on,
+            "user": order.user.id,
+            "completed_on": datetime.now(),
+            "payment_type": payment_type.id
+            }  
+        response = self.client.put(f'/api/orders/{self.order1.id}/complete', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        order_updated = Order.objects.get(pk=order.id)
+        self.assertEqual(order_updated.payment_type.id, data['payment_type'])
+        
+    # def test_add_product(self):
+    #     order = self.order1
+    #     product = self.product1
+    #     data = {
+    #         "order": order.id,
+    #         "product": product.id
+    #     }
+
+        
+    #     response = self.client.post(f'/api/products/{product.id}/add-to-order', data, format='json')
+
+    #     self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
