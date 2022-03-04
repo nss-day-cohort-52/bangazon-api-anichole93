d64efdde2070327962bd22c20c3c53d43547b5cd
Ashley Dickey <adickey93@gmail.com>
2022-03-04 14:08:14 -0600

    merge

--- tests/test_order.py
+++ tests/test_order.py
@@ -3,7 +3,6 @@ from rest_framework.test import APITestCase
 from rest_framework.authtoken.models import Token
 from django.core.management import call_command
 from django.contrib.auth.models import User
-
 from bangazon_api.models import Order, Product
 
 
@@ -44,4 +43,4 @@ class OrderTests(APITestCase):
         response = self.client.delete(f'/api/orders/{self.order1.id}')
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 
-    # TODO: Complete Order test
+    #TODO: Complete Order test
